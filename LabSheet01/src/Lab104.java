6871a99a4950e9e001941c1d0cb4f6a4f927aca2
nomana2548 <2413111101@tni.ac.th>
2024-12-11 08:43:34 +0700

    LabSheet01

---
+++ LabSheet01/src/Lab104.java
@@ -0,0 +1,9 @@
+
+public class Lab104 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LabSheet01/src/Lab105.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+public class Lab105 {
+    public static void main(String[] args) {
+        final double tax = 0.0625; 
+        String cdId;
+        String cdTitle;
+        double cdPrice;
+        int cdQuantity;
+        double cdSubtotal;
+        double cdTotal;
+
+        JOptionPane.showMessageDialog(null, "This program calculates the total cost of a CD order");
+
+        cdId = JOptionPane.showInputDialog("Please enter the ID of the CD");
+        cdTitle = JOptionPane.showInputDialog("Please enter the title of the CD");
+        cdPrice = Double.parseDouble(JOptionPane.showInputDialog("Please enter the price of the CD in U.S. dollars"));
+        cdQuantity = Integer.parseInt(JOptionPane.showInputDialog("Please enter the quantity to be purchased"));
+
+        cdSubtotal = cdPrice * cdQuantity;
+        cdTotal = cdSubtotal * (1 + tax);
+
+        String output = String.format(
+            "Summary of the transaction:\n" +
+            "CD ID: %s\n" +
+            "CD Title: %s\n" +
+            "CD Unit Price: $%.2f\n" +
+            "CD Quantity: %d\n" +
+            "Subtotal: $%.2f\n" +
+            "Tax rate: %.2f%%\n" +
+            "Total: $%.2f\n\n" +
+            "End of Program",
+            cdId, cdTitle, cdPrice, cdQuantity, cdSubtotal, tax * 100, cdTotal
+        );
+
+        JOptionPane.showMessageDialog(null, output);
+    }
+}
